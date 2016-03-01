274cd8b472331bc47d04fd5e52a2804570794225
duaraghav8@gmail <duaraghav8@gmail.com>
2016-03-01 22:59:53 +0530

    Created / Update Schemas

--- app/models.js
+++ app/models.js
@@ -1,16 +1,23 @@
 var mongoose = require ('mongoose'),
-    userSchema, userModel;
+    userSchema, userModel,
+    meetingSchema, meetingModel;
 
 userSchema = {
-  local: {
-    email: String,
-    password: String
-  },
-  facebook : {
-    id: String,
-    token: String,
-    displayName: String
-  }
+  local: { email: String, password: String },
+  facebook : { id: String, token: String, displayName: String },
+  upcomingMeetings: { type: Array },
+  invitations: { type: Array },
+  createdMeetings: { type: Array }
+};
+
+meetingSchema = {
+  creator: { type: String },
+  members: { type: Array },
+  slots: { type: Object },
+  venue: { type: String },
+  duration: { type: Number }
+  description: { type: String }
 };
 
 userModel = mongoose.model ('users', userSchema);
+meetingModel = mongoose.model ('meetings', meetingSchema);
